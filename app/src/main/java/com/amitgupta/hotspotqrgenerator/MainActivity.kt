b13557d7c45cef23e6ad5a1d65df1d939f796dcc
Amit Gupta <amitgupta@Amits-MacBook-Pro.local>
2020-02-10 18:52:37 +0545

    [wip][broadcast_hotspot]: wip hotspot broadcast receiving

--- app/src/main/java/com/amitgupta/hotspotqrgenerator/MainActivity.kt
+++ app/src/main/java/com/amitgupta/hotspotqrgenerator/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.amitgupta.hotspotqrgenerator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startActivity(Intent(this,DashBoardActivity::class.java))
     }
 }
