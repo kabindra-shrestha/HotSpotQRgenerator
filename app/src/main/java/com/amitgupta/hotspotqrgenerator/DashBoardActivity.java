19abf29c25d8b70a75a27afb5a301556674d04d5
Kabindra Shrestha <kabindra.shrestha.0wn@gmail.com>
2020-02-11 22:22:33 +0545

    worked on qr code

--- app/src/main/java/com/amitgupta/hotspotqrgenerator/DashBoardActivity.java
+++ app/src/main/java/com/amitgupta/hotspotqrgenerator/DashBoardActivity.java
@@ -4,29 +4,41 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class DashBoardActivity extends AppCompatActivity {
 
+    private ConstraintLayout hotspotInfoLayout;
+    private TextView hotspotErrorMessage;
+    private ImageView hotspotQRCode;
+    private TextView hotspotUsernamePassword;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        final TextView tv = findViewById(R.id.textview);
-        tv.setText("Changed");
+        hotspotInfoLayout = findViewById(R.id.hotspot_info_layout);
+        hotspotErrorMessage = findViewById(R.id.hotspot_error_message);
+        hotspotQRCode = findViewById(R.id.hotspot_qr_code);
+        hotspotUsernamePassword = findViewById(R.id.hotspot_username_password);
+
+        hotspotInfoLayout.setVisibility(View.GONE);
+        hotspotErrorMessage.setVisibility(View.GONE);
 
         IntentFilter filter = new IntentFilter();
         filter.addAction("android.net.wifi.WIFI_AP_STATE_CHANGED");
@@ -54,10 +66,25 @@ public class DashBoardActivity extends AppCompatActivity {
                         }
 
                         Toast.makeText(DashBoardActivity.this, "hotspotStateReceiver: info - " + wifiConfig.allowedKeyManagement + wifiConfig.SSID + " - " + wifiConfig.preSharedKey, Toast.LENGTH_SHORT).show();
-                        tv.setText("hotspotStateReceiver: info - " + wifiConfig.allowedKeyManagement + wifiConfig.SSID + " - " + wifiConfig.preSharedKey);
+
+                        String keyMgmt = "";
+                        String allowedkeyMgmt = wifiConfig.allowedKeyManagement + "";
+                        for (int i = 0; i < WifiConfiguration.KeyMgmt.strings.length; i++) {
+                            String keys = WifiConfiguration.KeyMgmt.strings[Integer.parseInt(allowedkeyMgmt.replace("{", "").replace("}", ""))];
+                            if (keys.contains("_"))
+                                keyMgmt = keys.split("_")[0];
+                            else
+                                keyMgmt = keys;
+                        }
+
+                        generateQRCode(keyMgmt, wifiConfig.SSID, wifiConfig.preSharedKey);
                     } else if (WifiManager.WIFI_STATE_DISABLED == state % 10) {
                         Toast.makeText(DashBoardActivity.this, "hotspotStateReceiver: disabled - " + state, Toast.LENGTH_SHORT).show();
-                        tv.setText("hotspotStateReceiver: disabled - " + state);
+
+                        hotspotErrorMessage.setText("Hotspot Disabled");
+
+                        hotspotInfoLayout.setVisibility(View.GONE);
+                        hotspotErrorMessage.setVisibility(View.VISIBLE);
                     }
                 }
             }
@@ -66,4 +93,17 @@ public class DashBoardActivity extends AppCompatActivity {
 
         registerReceiver(hotspotStateReceiver, filter);
     }
+
+    private void generateQRCode(String keyMgmt, String ssid, String preSharedKey) {
+        String text = "WIFI:T:" + keyMgmt + ";S:" + ssid + ";P:" + preSharedKey + ";";
+        Log.e("hotspotStateReceiver ", "info: " + text);
+
+        QRCodeUtils qrCodeUtils = new QRCodeUtils();
+        hotspotQRCode.setImageBitmap(qrCodeUtils.generateQRCode(text, 98, 98));
+        hotspotUsernamePassword.setText(ssid + " / " + preSharedKey);
+
+        hotspotInfoLayout.setVisibility(View.VISIBLE);
+        hotspotErrorMessage.setVisibility(View.GONE);
+    }
+
 }
---
+++ app/src/main/java/com/amitgupta/hotspotqrgenerator/QRCodeUtils.java
@@ -0,0 +1,87 @@
+package com.amitgupta.hotspotqrgenerator;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.util.Log;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static android.graphics.Color.BLACK;
+import static android.graphics.Color.TRANSPARENT;
+
+public class QRCodeUtils {
+
+    public Bitmap generateQRCode(String qrCode, int width, int height) {
+        // setting size of qr code
+        int smallestDimension = width < height ? width : height;
+
+        // setting parameters for qr code
+        String charset = "UTF-8";
+        Map<EncodeHintType, ErrorCorrectionLevel> hintMap = new HashMap<EncodeHintType, ErrorCorrectionLevel>();
+        hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+
+        return CreateQRCode(qrCode, charset, hintMap, smallestDimension, smallestDimension);
+    }
+
+    private Bitmap CreateQRCode(String qrCodeData, String charset, Map hintMap, int qrCodeheight, int qrCodewidth) {
+        try {
+            // generating qr code.
+            BitMatrix matrix = new MultiFormatWriter().encode(new String(qrCodeData.getBytes(charset), charset), BarcodeFormat.QR_CODE, qrCodewidth, qrCodeheight, hintMap);
+
+            // converting bitmatrix to bitmap
+            int width = matrix.getWidth();
+            int height = matrix.getHeight();
+            int[] pixels = new int[width * height];
+            // All are 0, or black, by default
+            for (int y = 0; y < height; y++) {
+                int offset = y * width;
+                for (int x = 0; x < width; x++) {
+                    // for black and white
+                    pixels[offset + x] = matrix.get(x, y) ? BLACK : TRANSPARENT;
+                    // for custom color
+//                    pixels[offset + x] = matrix.get(x, y) ? ResourcesCompat.getColor(getResources(),R.color.colorB,null) :WHITE;
+                }
+            }
+
+            // creating bitmap
+            Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+            bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+
+            // getting the logo
+//            Bitmap overlay = BitmapFactory.decodeResource(getResources(), R.drawable.launcher);
+            // returning bitmap to image view
+            return bitmap;
+        } catch (Exception er) {
+            Log.e("QrGenerate", er.getMessage());
+        }
+        return null;
+    }
+
+    public Bitmap mergeBitmaps(Bitmap overlay, Bitmap bitmap) {
+
+        int height = bitmap.getHeight();
+        int width = bitmap.getWidth();
+
+        Bitmap combined = Bitmap.createBitmap(width, height, bitmap.getConfig());
+        Canvas canvas = new Canvas(combined);
+        int canvasWidth = canvas.getWidth();
+        int canvasHeight = canvas.getHeight();
+
+        canvas.drawBitmap(bitmap, new Matrix(), null);
+
+        int centreX = (canvasWidth - overlay.getWidth()) / 2;
+        int centreY = (canvasHeight - overlay.getHeight()) / 2;
+        canvas.drawBitmap(overlay, centreX, centreY, null);
+
+        return combined;
+    }
+
+}
